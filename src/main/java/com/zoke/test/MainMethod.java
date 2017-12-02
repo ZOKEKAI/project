19c00f53b0039b24e2f1b3733b865cb7ee5d3d1d
ZOKEKAI <zokekai@gmail.com>
2017-12-03 01:45:58 +0800

    显示

--- src/main/java/com/zoke/test/MainMethod.java
+++ src/main/java/com/zoke/test/MainMethod.java
@@ -10,83 +10,102 @@ public class MainMethod {
 		b.setC(348);
 		b.setD(458);
 
+		run1();
+		run2();
+		run3();
+		run4();
+		run5();
+		run6();
+		run7();
+		run8();
+		run9();
+		run10();
+		run11();
+		run12();
+		run13();
+		run14();
+		run15();
+		run16();
+		run17();
+		run18();
+		run19();
+
 		System.out.println(b);
-		System.out.println("我是新增加的内容");
 	}
 
-	public void run1(){
+	public static void run1(){
 		System.out.println("1");
 	}
 
-	public void run2(){
+	public static void run2(){
 		System.out.println("2");
 	}
 
-	public void run3(){
+	public static void run3(){
 		System.out.println("3");
 	}
 
-	public void run4(){
+	public static void run4(){
 		System.out.println("4");
 	}
 
-	public void run5(){
+	public static void run5(){
 		System.out.println("5");
 	}
 
-	public void run6(){
+	public static void run6(){
 		System.out.println("6");
 	}
 
-	public void run7(){
+	public static void run7(){
 		System.out.println("7");
 	}
 
-	public void run8(){
+	public static void run8(){
 		System.out.println("8");
 	}
 
-	public void run9(){
+	public static void run9(){
 		System.out.println("9");
 	}
 
-	public void run10(){
+	public static void run10(){
 		System.out.println("10");
 	}
 
-	public void run11(){
+	public static void run11(){
 		System.out.println("11");
 	}
 
-	public void run12(){
+	public static void run12(){
 		System.out.println("12");
 	}
 
-	public void run13(){
+	public static void run13(){
 		System.out.println("13");
 	}
 
-	public void run14(){
+	public static void run14(){
 		System.out.println("14");
 	}
 
-	public void run15(){
+	public static void run15(){
 		System.out.println("15");
 	}
 
-	public void run16(){
+	public static void run16(){
 		System.out.println("16");
 	}
 
-	public void run17(){
+	public static void run17(){
 		System.out.println("17");
 	}
 
-	public void run18(){
+	public static void run18(){
 		System.out.println("18");
 	}
 
-	public void run19(){
+	public static void run19(){
 		System.out.println("19");
 	}
 
