353d03ab6b4da9fd5caf7b78366ed5199fcaa091
ZOKEKAI <zokekai@gmail.com>
2017-12-02 20:25:53 +0800

    delete run12

--- src/main/java/com/zoke/test/MainMethod.java
+++ src/main/java/com/zoke/test/MainMethod.java
@@ -68,9 +68,4 @@ public class MainMethod {
 		System.out.println("66666666666666");
 	}
 
-	public void run11(){
-		System.out.println("7777777777777");
-		System.out.println("7777777777777");
-	}
-
 }
